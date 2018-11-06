f712d7fb2de80dd30e40d7416e4d132c192458e8
AnneMarieHarm <amharm@earthlink.net>
2018-11-05 22:27:48 -0500

    AngularJS_TODO_Lab_1_Part_2 first commit

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -1,17 +1,28 @@
 "use strict";
 
+//Declare function
 function TodoController() {
+//vm stands for ViewModel
+//Declare variable
     const vm = this;
-
-    vm.tasks = [
+//Declare array
+    vm.tasks = [ 
         { task: "Handle physical Inbox", completed: true },
         { task: "Relax", completed: true },
         { task: "Handle regular 'in' and 'out' tasks", completed: true },
         { task: "Shop for groceries, etc.", completed: false },
         { task: "Work on Sunday", completed: true },
     ]
+//Add new todo to the array
+    vm.addTask = (newTodo) => {
+    vm.tasks.push({ task: newTodo, completed: false });
+    }
+//Remove task from the array
+    vm.removeTask = (removeTask) => {
+    vm.tasks.splice({ task: removeTask, completed: false });
+    }
 }
-
+//getter syntax
 angular
     .module("todoApp")
     .controller("TodoController", TodoController)
--- app/module/module.js
+++ app/module/module.js
@@ -1,4 +1,4 @@
 "use strict";
-
+//getter syntax
 angular.module("todoApp", []);
 
